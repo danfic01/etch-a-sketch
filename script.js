b9813bd69b53f3c197f708bf122a77169f16bc9a
danfic01 <danielrfic@gmail.com>
2024-04-29 19:52:54 -0400

    Add reset button and set static grid width
    Created resetButton function to add a rest button to the webpage,
    when clicked the user is prompted to enter a number to set the new grid resolution.
    Added event listener to check when reset button is clicked.
    Updated createGrid function to remove old grid when reset button is clicked.
    Updated creatGrid to size the squares so that the grid remains the same size regardless of number of squares.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+createResetButton();
 createGrid(16, 16);
 
 // add an event listener to check when the mouse enters the space of a square
@@ -12,12 +13,33 @@ document.addEventListener('mousemove', (event) => {
 
 })
 
+// add an event listener to check when the reset button is clicked
+document.addEventListener('click', (event) => {
+
+    let target = event.target; 
+
+    if(target.id === 'reset') {
+
+        let gridResolution = prompt('Please enter grid resolution'); // prompt the user to enter a number to size the new grid
+        while (Number(gridResolution) < 2 || Number(gridResolution) > 100) {
+            gridResolution = prompt('Please enter a positive integer no greater than 100'); // if the user enters a number less than 2 or greater than 100 prompt for re-entry
+        }
+        createGrid(gridResolution, gridResolution, true); // create a new grid sized to user specifications. Third argument sets reset to true so that the function will remove the old grid.
+    }
+})
+
 // declare a function to create a grid of squares on the webpage
-function createGrid (columns, rows) {
+function createGrid (columns, rows, reset=false) {
     
+    if (reset) {
+        document.body.removeChild(document.querySelector('.container'));
+    }
+
     const container = document.createElement('div'); // create the main container div
     container.setAttribute('class', 'container'); // set  class of div to container
     document.body.appendChild(container); // append the container div to the body element of the html document
+
+    const squareWidth = (800 - 8 * (columns -1)) / columns; // calculate the width of each individual square while the total width of the grid is 800px
 
     // for the number of columns given when the function is called create column containers to hold the sqaures 
     for(let i = 0; i < columns; i++) {
@@ -29,7 +51,17 @@ function createGrid (columns, rows) {
         for(let j = 0; j < rows; j++) {
             const square = document.createElement('div'); // create a grid square div
             square.setAttribute('class', 'square'); // set class of div to square
+            square.style.minWidth = `${squareWidth}px`; 
+            square.style.minHeight = `${squareWidth}px`; 
             column.appendChild(square); // append new square div as a child of the current column div
         }
     }    
+}
+
+// declare a function to create a button to reset the grid
+function createResetButton() {
+    const resetButton = document.createElement('button');
+    resetButton.setAttribute('id', 'reset');
+    resetButton.textContent = 'RESET';
+    document.body.appendChild(resetButton);
 }
